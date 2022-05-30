f66bff70f67878a97d0b9019474bfdc19638dffd
jimmy5217123 <jimmy5217123@gmail.com>
2022-05-30 15:57:02 +0800

    排程狀態

--- client/src/views/RealTime.vue
+++ client/src/views/RealTime.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-5">
-    <div style="display:flex;align-items:center">
+    <div style="display:flex; align-items:center">
       <div style="width:200px">
         <v-text-field label="search 案場">
           <v-icon slot="append" color="red">
@@ -9,11 +9,35 @@
         </v-text-field>
       </div>
       <div class="mx-10">資料筆數: 22</div>
+      <div style="margin-left:auto">
+        <div style="font-weight:700">
+          <span class="mx-0">排程狀態:</span>
+          <span class="mx-5">
+            <v-icon color='green' small class="mb-1 mx-1">
+              {{'mdi-checkbox-marked-circle'}}
+            </v-icon>
+            <span>運轉中</span>
+          </span>
+          <span class="mx-5">
+            <v-icon color='yellow' small class="mb-1 mx-1">
+              {{'mdi-stop-circle'}}
+            </v-icon>
+            <span>待機中</span>
+          </span>
+          <span class="mx-5">
+            <v-icon color='red' small class="mb-1 mx-1">
+              {{'mdi-close-octagon'}}
+            </v-icon>
+            <span>停機中</span>
+          </span>
+        </div>
+        <!-- <div>案場狀態</div> -->
+      </div>
     </div>
     <v-card class="mb-5 cardBox" outlined elevation="2" v-for="x in 20" :key="x" @click="toInfoPage">
       <table style="width: 100%">
         <tr>
-          <th v-for="i in factoryData" :key="i">{{i}}</th>
+          <th v-for="i in factoryDataTitle" :key="i">{{i}}</th>
         </tr>
         <tr>
           <td>儲能案場</td>
@@ -47,7 +71,7 @@
     name: 'HelloWorld',
 
     data: () => ({
-      factoryData: ['案場名稱', '建置容量(kWh)', '饋線容量(kW)​', 'SOC(%)', 'SOH(%)​', '充放電(+/-kW)​', '轉換效率(%)​', '直流功率(+/-kW)', '排程運轉中​', '案場狀態']
+      factoryDataTitle: ['案場名稱', '建置容量(kWh)', '饋線容量(kW)​', 'SOC(%)', 'SOH(%)​', '充放電(+/-kW)​', '轉換效率(%)​', '直流功率(+/-kW)', '排程運轉中​', '案場狀態']
     }),
     methods: {
       toInfoPage () {
